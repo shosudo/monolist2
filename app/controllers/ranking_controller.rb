aaee1a8508456c5447c6e5717583ed3066409961
Sho Sudo <sudosho1101@gmail.com>
2017-02-05 07:59:20 +0000

    render action when logged_in

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -3,7 +3,9 @@ class RankingController < ApplicationController
     
     def have
         ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+        #Haveグループの中から、item_idの数をカウントして並べ替え
         @items = Item.find(ids).sort_by do |element|
+        #持ってきたものから並べ替え
             ids.index(element.id)
         end
     end
